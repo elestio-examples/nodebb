c334942e853929024e34aeb92ebd92482a6ba614
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-14 18:21:40 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,5 +1,14 @@
 #set env vars
-#set -o allexport; source .env; set +o allexport;
+set -o allexport; source .env; set +o allexport;
+
+sed -i "s/INSTANCE_DOMAIN/$DOMAIN/g" ./scripts/mongo_setup.sh
+
+sed -i "s/SOFTWARE_PASSWORD/$ADMIN_PASSWORD/g" ./scripts/mongo_setup.sh
+
+mkdir -p ./auth;
+echo $(openssl rand -hex 20) > ./auth/key;
+chmod 600 ./auth/key 
+
 
 
 mkdir -p ./mongo
