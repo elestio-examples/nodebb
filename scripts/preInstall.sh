497943947673333581534b56ecfed3397e964f25
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-21 14:14:50 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,10 +2,12 @@
 set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./build
-mkdir -p ./uploads
+mkdir -p ./nodebb-data
+mkdir -p ./nodebb-files
 mkdir -p ./node_modules
 chmod 777 ./build
-chmod 777 ./uploads
+chmod 777 ./nodebb-data
+chmod 777 ./nodebb-files
 chmod 777 ./node_modules
 
 chmod +x ./entrypoint.sh
