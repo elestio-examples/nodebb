86da98434e1d5a47405e1e6874d7bf18b8779a77
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-22 13:36:44 +0300

    adding tests

--- tests/build.sh
+++ tests/build.sh
@@ -2,7 +2,6 @@
 sed -i "s~FROM --platform=\$BUILDPLATFORM node:lts as npm~FROM node:lts as npm~g" ./Dockerfile;
 sed -i 's~CMD test -n "${SETUP}" && ./nodebb setup || node ./nodebb build; node ./nodebb start~CMD ./entrypoint.sh \&\& node ./nodebb build; node ./nodebb start~g' ./Dockerfile
 
-
-
-
 docker build . --tag elestio4test/nodebb:latest
+
+sed -i "s~FROM node:lts as npm~FROM --platform=\$BUILDPLATFORM node:lts as npm~g" ./Dockerfile;
