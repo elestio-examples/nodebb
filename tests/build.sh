76e056c86745f519c117d8177a5d7db9b2c76fc6
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-12-28 10:25:49 +0200

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
-sed -i 's~CMD test -n "${SETUP}" && ./nodebb setup || node ./nodebb build; node ./nodebb start~CMD ./entrypoint.sh \&\& node ./nodebb build; node ./nodebb start~g' ./Dockerfile
+# sed -i 's~CMD test -n "${SETUP}" && ./nodebb setup || node ./nodebb build; node ./nodebb start~CMD ./entrypoint.sh \&\& node ./nodebb build; node ./nodebb start~g' ./Dockerfile
 
 docker buildx build . --output type=docker,name=elestio4test/nodebb:latest | docker load
